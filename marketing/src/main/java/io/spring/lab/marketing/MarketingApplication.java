1e9d35e107857c0c2b3e5ada6f80c078c4c46ec3
Sebastian Kramer <sebastian.kramer@wp.pl>
2018-09-28 21:12:21 +0200

    fixed specials initialization

--- marketing/src/main/java/io/spring/lab/marketing/MarketingApplication.java
+++ marketing/src/main/java/io/spring/lab/marketing/MarketingApplication.java
@@ -4,13 +4,15 @@ import java.math.BigDecimal;
 
 import io.spring.lab.marketing.special.Special;
 import io.spring.lab.marketing.special.SpecialRepository;
+import lombok.AllArgsConstructor;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication(scanBasePackages = "io.spring.lab")
+@SpringBootApplication
 public class MarketingApplication {
 
     public static void main(String[] args) {
@@ -18,16 +20,16 @@ public class MarketingApplication {
     }
 }
 
-@Configuration
-class RepositoryInitializer {
+@Component
+@AllArgsConstructor
+class RepositoryInitializer implements ApplicationRunner {
     private SpecialRepository specials;
 
-    public ApplicationRunner setSpecials() {
-        return args -> {
-            specials.save(new Special(null, 1L, 3, BigDecimal.valueOf(70.0)));
-            specials.save(new Special(null, 2L, 2, BigDecimal.valueOf(15.0)));
-            specials.save(new Special(null, 3L, 4, BigDecimal.valueOf(60.0)));
-            specials.save(new Special(null, 4L, 2, BigDecimal.valueOf(40.0)));
-        };
+    @Override
+    public void run(ApplicationArguments args) {
+        specials.save(new Special(null, 1L, 3, BigDecimal.valueOf(70.0)));
+        specials.save(new Special(null, 2L, 2, BigDecimal.valueOf(15.0)));
+        specials.save(new Special(null, 3L, 4, BigDecimal.valueOf(60.0)));
+        specials.save(new Special(null, 4L, 2, BigDecimal.valueOf(40.0)));
     }
 }
