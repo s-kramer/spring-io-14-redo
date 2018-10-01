719a25be2f19533f53e46a86a2c47aa38a8a6f1f
Sebastian Kramer <sebastian.kramer@wp.pl>
2018-10-01 22:47:08 +0200

    added spock mvc test

--- warehouse/src/test/groovy/io/spring/lab/warehouse/item/ItemControllerTest.groovy
+++ warehouse/src/test/groovy/io/spring/lab/warehouse/item/ItemControllerTest.groovy
@@ -1,19 +1,17 @@
 package io.spring.lab.warehouse.item
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.spring.lab.warehouse.ItemDataInitializator
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.context.annotation.Bean
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 
+import static org.hamcrest.Matchers.hasItems
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
@@ -23,10 +21,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ItemControllerTest extends Specification {
 
     @Autowired
-    MockMvc mockMvc;
+    MockMvc mockMvc
 
     @Autowired
     ObjectMapper objectmaper
+
+    def "should find all items in the repository"() {
+        expect:
+        mockMvc.perform(get("/item")
+                .contentType(APPLICATION_JSON_UTF8))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath('$.length()').value(4))
+                .andExpect(jsonPath('$.[*].name').value(hasItems('A', 'B', 'C', 'D')))
+    }
 
     def "should create item and add it to repository"() {
         given:
@@ -48,19 +55,5 @@ class ItemControllerTest extends Specification {
         result.andExpect(jsonPath('$.name').value(('itemName')))
         result.andExpect(jsonPath('$.count').value((10)))
         result.andExpect(jsonPath('$.price').value(13.5))
-    }
-
-    @TestConfiguration
-    static class SpringSpecConfig {
-
-        @Autowired
-        ItemRepository items
-
-        @Bean
-        ApplicationRunner init() {
-            return { args ->
-                ItemDataInitializator.itemsTestData(items)
-            } as ApplicationRunner
-        }
     }
 }
