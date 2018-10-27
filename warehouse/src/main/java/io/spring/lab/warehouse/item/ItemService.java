6c833a4ccbf8d5a401528a0c3890865b0880dbfe
Sebastian Kramer <sebastian.kramer@wp.pl>
2018-10-27 13:57:31 +0200

    added grafana and prometheus

--- warehouse/src/main/java/io/spring/lab/warehouse/item/ItemService.java
+++ warehouse/src/main/java/io/spring/lab/warehouse/item/ItemService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.apache.commons.lang3.Validate;
 import org.springframework.stereotype.Component;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -13,22 +14,29 @@ import lombok.AllArgsConstructor;
 public class ItemService {
 
 	private final ItemRepository items;
+	private final MeterRegistry meterRegistry;
 
 	public Item findOne(long id) {
 		Optional<Item> item = items.findOne(id);
+        meterRegistry.counter("item.service.search." + id + ".count").increment();
 		return item.orElseThrow(() -> new ItemNotFound(id));
 	}
 
 	public Item findMostExpensiveItem() {
-        return items.findMostExpensiveItem().orElseThrow(() -> new IllegalArgumentException("No items in repository"));
+        final Optional<Item> mostExpensiveItem = items.findMostExpensiveItem();
+        meterRegistry.counter("item.service.search.by.expensiveness.count").increment();
+        return mostExpensiveItem.orElseThrow(() -> new IllegalArgumentException("No items in repository"));
     }
 
 	public List<Item> findAll() {
+        meterRegistry.counter("item.service.search.all.count").increment();
 		return items.findAll();
 	}
 
 	public Item create(Item item) {
-		return items.save(item);
+        final Item save = items.save(item);
+        meterRegistry.counter("item.service.creation.count").increment();
+        return save;
 	}
 
 	public Item update(ItemUpdate changes) {
@@ -36,7 +44,9 @@ public class ItemService {
 
 		Item item = findOne(changes.getId());
 		item.update(changes);
-		return items.save(item);
+        final Item save = items.save(item);
+        meterRegistry.counter("item.service.update.count").increment();
+        return save;
 	}
 
 	public Item updateStock(ItemStockUpdate changes) {
@@ -45,11 +55,13 @@ public class ItemService {
 		Item item = findOne(changes.getId());
 		item.updateStock(changes);
 
+        meterRegistry.counter("item.service.stock.update.count").increment();
 		return items.save(item);
 	}
 
     public void deleteItem(long itemId) {
         items.delete(itemId);
+        meterRegistry.counter("item.service.deletion.count").increment();
     }
 }
 
